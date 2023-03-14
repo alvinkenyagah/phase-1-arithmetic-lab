ed6639b01f8faad015c9883ed956294ee9a82563
alvinkenyagah <alvin47k@gmail.com>
2023-03-14 13:41:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 
-// Write your code here
+
+let num1=2;
+let num2 =31;
+let multiply = num1 * num2;
+// return multiplied;
+console.log(multiply)
+
+
+let random = Math.ceil(Math.random() *100)
+
+console.log(random)
+
+
+
+
+let num3=1345
+let num4=4.5
+
+let mod = num3%num4
+
+console.log(mod)
+
+
+
+let numbers = [10, 5, 20, 15];
+let max = Math.max(...numbers);
+
+console.log(highestNumber);
